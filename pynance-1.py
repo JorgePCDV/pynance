5f753ab490790e859f00a23800448bd3e1566ff4
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-01-25 18:05:24 +0000

    save AMZN data

--- pynance-1.py
+++ pynance-1.py
@@ -1,5 +1,11 @@
-# import pandas_datareader.data as pdr
-# import datetime as dt
+import pandas_datareader.data as pdr
+import datetime as dt
 
-print("hola python")
 ticker = "AMZN"
+start_date = dt.date.today() - dt.timedelta(365)
+end_date = dt.date.today()
+
+data = pdr.get_data_yahoo(ticker, start_date, end_date)
+data.to_csv('out.csv', encoding='utf-8', index=False)
+
+print(data)
