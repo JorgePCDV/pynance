4487fb331ee44aac1d25f1d3a7124210160d377d
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2020-01-25 19:48:35 +0000

    Single stock data

--- single-stock-data.py
+++ single-stock-data.py
@@ -5,7 +5,7 @@ ticker = "AMZN"
 start_date = dt.date.today() - dt.timedelta(365)
 end_date = dt.date.today()
 
-data = pdr.get_data_yahoo(ticker, start_date, end_date)
+data = pdr.get_data_yahoo(ticker, start_date, end_date, interval='m')
 data.to_csv('out.csv', encoding='utf-8', index=False)
 
 print(data)
